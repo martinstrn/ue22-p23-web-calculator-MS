2dc8182a58d9a4c36cbd3bbbbbe2f8375ca0d1af
martinstrn <martin.sterin1@gmail.com>
2024-03-21 20:52:31 +0100

    first commit with initial at the beginning

--- subject/script.js
+++ subject/script.js
@@ -1,4 +1,6 @@
 // NOTE: 
+
+// THIS IS Martin STERIN code
 // This is the starter file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
 
 // # START EDITING YOUR JAVASCRIPT HERE
